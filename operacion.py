3aae7eafe819e97277880c46d2407b9f085987d5
josivilches <jl.vilches@alumno.etec.um.edu.ar>
2022-10-20 08:55:37 -0300

    hola profe

---
+++ operacion.py
@@ -0,0 +1,16 @@
+import math
+n1 = 0
+n2 = 0
+
+class Operacion:
+    def suma(self, n1, n2):
+        return n1 + n2
+    def multi(self, n1, n2):
+        return n1 * n2
+    def divi(self, n1, n2):
+        return n1 / n2
+    def resta(self, n1, n2):
+        return n1 - n2
+
+
+
